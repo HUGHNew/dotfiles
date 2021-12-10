682cfc520695d656838f2a67efa5e9e3c9d1e4e2
Hugh <1728902978@qq.com>
2021-12-10 15:51:18 +0800

    add scripts (ocr)

---
+++ installer/install-ocr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## install tesseract-ocr
+sudo add-apt-repository ppa:alex-p/tesseract-ocr
+
+sudo apt update
+
+sudo apt install tesseract-ocr imagemagick gnome-screenshot xclip
+
+wget https://github.com/tesseract-ocr/tessdata/raw/main/chi_sim.traineddata -O chi_sim.traineddata
+sudo mv chi_sim.traineddata /usr/share/tesseract-ocr/4.00/tessdata/
