a512c89a938ca4d695ff9baedb6f26311202912a
Hugh <hugh.lpd@gmail.com>
2022-07-08 23:27:54 +0800

    add git fragment

--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -1,10 +1,10 @@
 # proxy enable on Gnome
 local proxy_path="/home/$(whoami)/clash"
 function set-proxy(){
-    export all_proxy="socks://127.0.0.1:7891/"
+    export all_proxy="socks5://127.0.0.1:7891/"
     export http_proxy="http://127.0.0.1:7890/"
     export https_proxy="http://127.0.0.1:7890/"
-    export ALL_PROXY="socks://127.0.0.1:7891/"
+    export ALL_PROXY="socks5://127.0.0.1:7891/"
     export HTTPS_PROXY="http://127.0.0.1:7890/"
     export HTTP_PROXY="http://127.0.0.1:7890/"
 }
