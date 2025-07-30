2152528aabda1d6e209e557a9831acd6733a923b
Hugh <hugh.lpd@gmail.com>
2025-07-30 20:57:03 +0800

    add export for PATH and flutter

--- sh/export.sh
+++ sh/export.sh
@@ -1,10 +1,15 @@
 ###### PATH #######
+[ -e ~/.local/bin ] && _EP=$_EP:~/.local/bin
+[ -e /usr/local/cuda ] && _EP=$_EP:/usr/local/cuda/bin
 
-
+export PATH=$_EP:$PATH
 ###################
 
 #### RUST #######
 export RUSTUP_DIST_SERVER="https://rsproxy.cn"
 export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
+#### Flutter ####
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 #################
 
