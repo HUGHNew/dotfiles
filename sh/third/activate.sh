5afb51280c853081a8e4cc1f4a84430d028cc54e
Hugh <hugh.lpd@gmail.com>
2026-03-27 22:08:45 +0800

    fixup for startup

--- sh/third/activate.sh
+++ sh/third/activate.sh
@@ -1,11 +1,11 @@
 case "$(basename $SHELL)" in
   "bash")
     [ -e /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-    command -v fzf 2>&1 && source <(fzf --bash)
+    command -v fzf 2>&1 >/dev/null && source <(fzf --bash)
   ;;
   "zsh")
     [ -e /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
-    command -v fzf 2>&1 && source <(fzf --zsh)
+    command -v fzf 2>&1 >/dev/null && source <(fzf --zsh)
   ;;
   *);;
 esac
